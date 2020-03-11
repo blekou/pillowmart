0270e2bc7d946775309af1ef60f199259a165556
elysee-01 <61144357+elysee-01@users.noreply.github.com>
2020-03-11 09:38:40 +0000

    Initial commit

---
+++ pillow/views.py
@@ -0,0 +1,94 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    datas = {
+
+    }
+    return render(request, "index.html", datas)
+
+
+def about(request):
+    datas = {
+
+    }
+    return render(request, "about.html", datas)
+
+
+def blog(request):
+    datas = {
+
+    }
+    return render(request, "blog.html", datas)
+
+
+def cart(request):
+    datas = {
+
+    }
+    return render(request, "cart.html", datas)
+
+
+def checkout(request):
+    datas = {
+
+    }
+    return render(request, "checkout.html", datas)
+
+
+def confirmation(request):
+    datas = {
+
+    }
+    return render(request, "confirmation.html", datas)
+
+
+def contact(request):
+    datas = {
+
+    }
+    return render(request, "contact.html", datas)
+
+
+def elements(request):
+    datas = {
+
+    }
+    return render(request, "elements.html", datas)
+
+
+def login(request):
+    datas = {
+
+    }
+    return render(request, "login.html", datas)
+
+
+def main(request):
+    datas = {
+
+    }
+    return render(request, "main.html", datas)
+
+
+def product_list(request):
+    datas = {
+
+    }
+    return render(request, "product_list.html", datas)
+
+
+def single_blog(request):
+    datas = {
+
+    }
+    return render(request, "single-blog.html", datas)
+
+
+def single_product(request):
+    datas = {
+
+    }
+    return render(request, "single-product.html", datas)
